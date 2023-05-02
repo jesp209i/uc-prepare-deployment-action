e8ef91977c6f50515fc1ff76a7fb2858be0db25a
Jesper Madsen <jam@umbraco.dk>
2023-05-02 19:35:12 +0200

    initial action

---
+++ create_deployment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Set variables
+url="https://apim-dev-global.azure-api.net/global/project/cicd-prototype/api/public/projects/$1/deployments"
+token="$2"
+commitMessage="$3"
+
+# Define function to call API and check status
+function call_api {
+  echo "$url"
+  response=$(curl -s -X POST $url \
+    -H "Ocp-Apim-Subscription-Key: $token" \
+    -H "Content-Type: application/json" \
+    -d "{\"commitMessage\":\"$commitMessage\"}")
+
+  status=$(echo "$response" | jq -r '.deploymentState')
+  deployment_id=$(echo "$response" | jq -r '.deploymentId')
+  if [[ $status != "Pending" ]]; then
+    echo "Unexpected status: $status"
+    exit 1
+  fi
+
+  echo "Deployment started successfully -> $deployment_id"
+}
+
+call_api
+
+echo "DEPLOYMENT_ID=$deployment_id" >> $GITHUB_OUTPUT
