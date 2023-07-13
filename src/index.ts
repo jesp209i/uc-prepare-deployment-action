5f736ac191aae714839ae744fb23c3542a925228
Jesper Madsen <jam@umbraco.dk>
2023-07-14 00:42:46 +0200

    ts

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { getInput, setOutput, setFailed } from '@actions/core';
+import {HttpClient, MediaTypes, Headers, HttpClientError} from '@actions/http-client';
+
+async function run() {
+    try {
+        const projectAlias = getInput('project-alias');
+        const apiKey = getInput('api-key');
+        const commitMessage = getInput('commit-message');
+
+        const url = `https://api-internal.umbraco.io/projects/${projectAlias}/deployments/`
+
+        const requestBody = {
+            "commitMessage": commitMessage
+        };
+
+        const headers = {
+            [Headers.ContentType]: MediaTypes.ApplicationJson,
+            "Umbraco-Api-Key": apiKey
+        };
+
+        const client = new HttpClient();
+
+        const response = await client.post(url, JSON.stringify(requestBody), headers);
+        const responseBody = await response.readBody()
+        const obj = JSON.parse(responseBody)
+
+        setOutput('DEPLOYMENT_ID', obj.deploymentId);
+
+    } catch (error: unknown) {
+        console.log(error);
+        setFailed("Got an error while trying to prepare a deployment")
+    }
+}
+
+
+
+run();
